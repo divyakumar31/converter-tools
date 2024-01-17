73fcc798db212f3ddb5367b0b9ff6d49a21d8bab
Divya Patel <121997327+divyakumar31@users.noreply.github.com>
2024-01-17 16:06:04 +0000

    UI Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let decimal = document.querySelector('#decimal');
 let binary = document.querySelector('#binary');
 let octal = document.querySelector('#octal');
-let hexadecimal = document.querySelector('#hexadecimal');
 
 decimal.addEventListener("input", () => {
     decimal.value = decimal.value.replace(/[^0-9]/g, '');
@@ -14,7 +13,6 @@ decimal.addEventListener("input", () => {
     let decimalval = parseInt(decimal.value);
     binary.value = isNaN(decimalval) ? '' : decimalval.toString(2);  // Output: "1010"
     octal.value = isNaN(decimalval) ? '' : decimalval.toString(8);
-    hexadecimal.value = isNaN(decimalval) ? '' : decimalval.toString(16);
 });
 
 binary.addEventListener('input', (e) => {
@@ -28,7 +26,6 @@ binary.addEventListener('input', (e) => {
     let binaryValue = parseInt(binary.value, radix = 2);
     decimal.value = isNaN(binaryValue) ? '' : binaryValue.toString(10);
     octal.value = isNaN(binaryValue) ? '' : binaryValue.toString(8);
-    hexadecimal.value = isNaN(binaryValue) ? '' : binaryValue.toString(16);
 });
 
 octal.addEventListener('input', (e) => {
@@ -42,21 +39,6 @@ octal.addEventListener('input', (e) => {
     let octalValue = parseInt(octal.value, radix = 8);
     decimal.value = isNaN(octalValue) ? '' : octalValue.toString(10);
     binary.value = isNaN(octalValue) ? '' : octalValue.toString(2);
-    hexadecimal.value = isNaN(octalValue) ? '' : octalValue.toString(16);
-});
-
-hexadecimal.addEventListener('input', (e) => {
-    hexadecimal.value = hexadecimal.value.replace(/[^0-9a-fA-F]/g, '');
-
-    // Prevent the default behavior for non-numeric key presses
-    if (!/^[0-9a-fA-F]*$/.test(hexadecimal.value)) {
-        e.preventDefault();
-    }
-
-    let hexadecimalValue = parseInt(hexadecimal.value, radix = 16);
-    decimal.value = isNaN(hexadecimalValue) ? '' : hexadecimalValue.toString(10);
-    binary.value = isNaN(hexadecimalValue) ? '' : hexadecimalValue.toString(2);
-    octal.value = isNaN(hexadecimalValue) ? '' : hexadecimalValue.toString(8);
 });
 
 document.addEventListener('paste', (e) => {
